39e34d8dfdc197fe9c510a9d265d90fc6d8b6133
jeanlsilva <jeanslucasilva@gmail.com>
2024-04-23 14:35:38 -0300

    feat: address form validation

--- src/components/ShippingSummary.tsx
+++ src/components/ShippingSummary.tsx
@@ -1,8 +1,21 @@
+import { Address } from "@/types/address";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
 export function ShippingSummary(props: any) {
     const router = useRouter();
+    const schema = z.object({
+        firstLine: z.string().min(6, "Campo deve ter no mínimo 6 caracteres"),
+        streetName: z.string().min(6, "Campo deve ter no mínimo 6 caracteres"),
+        postcode: z.string().min(4, "Campo deve ter no mínimo 4 caracteres")
+    });
+    const { register, handleSubmit, watch, formState: { errors }, } = useForm<Address>({ resolver: zodResolver(schema) });
+    const onSubmit: SubmitHandler<Address> = (data) => {
+        console.log({ data })
+    }
 
     return (
         <div className="bg-gray-100 rounded-md h-100 flex-1 px-10 pt-9 pb-20">
@@ -21,19 +34,22 @@ export function ShippingSummary(props: any) {
                         <option>123, Electric avenue</option>
                     </select>
                 </div>
-                <form>
-                    <div className="flex flex-col mt-4">
-                        <label htmlFor="address1">First line of address</label>
-                        <input type="text" name="address1" />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex flex-col pb-6 relative">
+                        <label htmlFor="firstLine">First line of address</label>
+                        <input type="text" {...register("firstLine")} />
+                        <span className="text-sm text-red-600 absolute bottom-0">{errors.firstLine?.message}</span>
                     </div>
-                    <div className="flex flex-col mt-4">
-                        <label htmlFor="address2">Street name</label>
-                        <input type="text" name="address2" />
+                    <div className="flex flex-col pb-6 relative">
+                        <label htmlFor="streetName">Street name</label>
+                        <input type="text" {...register("streetName")} />
+                        <span className="text-sm text-red-600 absolute bottom-0">{errors.streetName?.message}</span>
                     </div>
-                    <div className="flex gap-6 mt-4">
+                    <div className="flex gap-6 pb-6 relative">
                         <div className="flex flex-col flex-1">
                             <label htmlFor="postcode">Postcode</label>
-                            <input type="text" name="postcode" />
+                            <input type="text" {...register("postcode")} />
+                            <span className="text-sm text-red-600 absolute bottom-0">{errors.postcode?.message}</span>
                         </div>
                         <div className="flex flex-col flex-1">
                             <label htmlFor="shippingType">
@@ -44,16 +60,16 @@ export function ShippingSummary(props: any) {
                             </select>
                         </div>
                     </div>
-                </form>
-                <div className="border-t border-y-gray-300 flex justify-end gap-4 mt-8 pt-9">
+                    <div className="border-t border-y-gray-300 flex justify-end gap-4 mt-8 pt-9">
                     <button className="bg-transparent w-1/4">Cancel order</button>
                     <button 
                         className="bg-cyan-600 text-white rounded-lg w-1/3 py-4"
-                        onClick={() => router.push('/payment')}
+                        type="submit"
                     >
                         Payment
                     </button>
                 </div>
+                </form>
             </div>
         </div>
     )
