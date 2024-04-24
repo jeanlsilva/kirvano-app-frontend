96b12d86ce8fdb98eadea45a7bfac9a48d1fc997
jeanlsilva <jeanslucasilva@gmail.com>
2024-04-23 22:03:26 -0300

    chore: redirect to shipping page on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/shipping');
+  })
   return (
     <main>
-      <h1>Kirvano App</h1>
     </main>
   );
 }
